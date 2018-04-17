5f4f6298549045cb35723f8c04e79991b17364bf
Seale Rapolai <srapolai@retrorabbit.co.za>
2018-04-17 22:06:25 +0200

    completed main menu structure

--- startup.py
+++ startup.py
@@ -14,17 +14,66 @@ class MyFrame(wx.Frame):
 
     def __init__(self, title, pos, size):
         wx.Frame.__init__(self, None, -1, title, pos, size)
-        menuFile = wx.Menu()
-        menuFile.Append(1, "&About...")
-        menuFile.AppendSeparator()
-        menuFile.Append(2, "E&xit")
         menuBar = wx.MenuBar()
+
+        #File Submenu
+        menuFile = wx.Menu()
+        menuFile.Append(1, "New Project...")
+        menuFile.Append(2, "Open...")
+        menuFile.Append(3, "&Save")
+        menuFile.Append(4, "&Save As...")
+        menuFile.AppendSeparator()
+        menuFile.Append(5, "&Settings")
+        menuFile.AppendSeparator()
+        menuFile.Append(6, "&Exit")
         menuBar.Append(menuFile, "&File")
+
+        #Edit Submenu
+        menuEdit = wx.Menu()
+        menuEdit.Append(8, "&Undo...")
+        menuEdit.Append(9, "&Redo...")
+        menuEdit.AppendSeparator()
+        menuEdit.Append(10, "&Copy")
+        menuEdit.Append(11, "&Cut")
+        menuEdit.Append(12, "&Paste")
+        menuEdit.AppendSeparator()
+        menuEdit.Append(13, "&Font...")
+        menuBar.Append(menuEdit, "&Edit")
+
+        #View Submenu
+        menuView = wx.Menu()
+        menuBar.Append(menuView, "&View")
+
+        #Find Submenu
+        menuFind = wx.Menu()
+        menuFind.Append(14, "&Individual")
+        menuFind.Append(15, "&Group")
+        menuBar.Append(menuFind, "&Find")
+
+        #Export Submenu
+        menuExport = wx.Menu()
+        menuExport.Append(16, "&PDF")
+        menuExport.Append(17, "&PNG")
+        menuExport.Append(18, "&SVG")
+        menuBar.Append(menuExport, "&Export")
+
+        #Help Submenu
+        menuHelp = wx.Menu()
+        menuHelp.Append(19, "&Documentation (link)")
+        menuHelp.Append(19, "&FAQ (link)")
+        menuHelp.AppendSeparator()
+        menuHelp.Append(20, "&Report Issue")
+        menuHelp.Append(20, "&Feedback")
+        menuHelp.AppendSeparator()
+        menuHelp.Append(21, "&Check for Updates...")
+        menuHelp.Append(1, "&About")
+        menuBar.Append(menuHelp, "&Help")
+
         self.SetMenuBar(menuBar)
         self.CreateStatusBar()
-        self.SetStatusText("Welcome to wxPython!")
+        self.SetStatusText("Welcome to Genesis!")
         self.Bind(wx.EVT_MENU, self.OnAbout, id=1)
-        self.Bind(wx.EVT_MENU, self.OnQuit, id=2)
+        self.Bind(wx.EVT_MENU, self.OnQuit, id=6)
 
     def OnQuit(self, event):
         self.Close()
