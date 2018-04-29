ebfe93d0f64ff7778293908b2a1e2f555e4ada2e
Seale Rapolai <srapolai@retrorabbit.co.za>
2018-04-29 20:04:28 +0200

    Created event listeners for browse for input files.

--- views/plotGraph.py
+++ views/plotGraph.py
@@ -1,9 +1,14 @@
 import wx
+from views import browseFiles
+from helpers import identityCodes
 
 
 class PlotGraphFrame(wx.Frame):
 
     title = "Input files: "
+    dataFile = None
+    famFile = None
+    phenotypeFile = None
 
     def __init__(self, parent, type):
         wx.Frame.__init__(self, parent, wx.ID_ANY, self.title, size=(500, 250))
@@ -15,11 +20,13 @@ class PlotGraphFrame(wx.Frame):
         hbox1 = wx.BoxSizer(wx.HORIZONTAL)
         st1 = wx.StaticText(self.Panel, label='Data File: ', size=(70, 25))
         hbox1.Add(st1, flag=wx.RIGHT, border=8)
-        tc = wx.TextCtrl(self.Panel)
-        hbox1.Add(tc, proportion=1)
-        browse_file1 = wx.Button(self.Panel, label='...', size=(30, 25))
+        self.dataTC = wx.TextCtrl(self.Panel)
+        hbox1.Add(self.dataTC, proportion=1)
+        browse_file1 = wx.Button(self.Panel, id=identityCodes.PLOT_BROWSE_DATA, label='...', size=(30, 25))
         hbox1.Add(browse_file1, flag=wx.LEFT, border=8)
         self.vBox.Add(hbox1, flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, border=10)
+
+        self.Bind(wx.EVT_BUTTON, self.OnBrowseData, id=identityCodes.PLOT_BROWSE_DATA)
 
         if type == "Admix":
             self.InitAdmixUI()
@@ -30,9 +37,9 @@ class PlotGraphFrame(wx.Frame):
         hbox2 = wx.BoxSizer(wx.HORIZONTAL)
         st1 = wx.StaticText(self.Panel, label='Fam File: ', size=(70,25))
         hbox2.Add(st1, flag=wx.RIGHT, border=8)
-        tc = wx.TextCtrl(self.Panel)
-        hbox2.Add(tc, proportion=1)
-        browse_file2 = wx.Button(self.Panel, label='...', size=(30, 25))
+        self.famTC = wx.TextCtrl(self.Panel)
+        hbox2.Add(self.famTC, proportion=1)
+        browse_file2 = wx.Button(self.Panel, id=identityCodes.PLOT_BROWSE_FAM, label='...', size=(30, 25))
         hbox2.Add(browse_file2, flag=wx.LEFT, border=8)
         self.vBox.Add(hbox2, flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, border=10)
 
@@ -40,14 +47,14 @@ class PlotGraphFrame(wx.Frame):
         hbox3 = wx.BoxSizer(wx.HORIZONTAL)
         st1 = wx.StaticText(self.Panel, label='Phenotype: ', size=(70,25))
         hbox3.Add(st1, flag=wx.RIGHT, border=8)
-        tc = wx.TextCtrl(self.Panel)
-        hbox3.Add(tc, proportion=1)
-        browseFile3 = wx.Button(self.Panel, label='...', size=(30, 25))
+        self.phoneTC = wx.TextCtrl(self.Panel)
+        hbox3.Add(self.phoneTC, proportion=1)
+        browseFile3 = wx.Button(self.Panel, id=identityCodes.PLOT_BROWSE_PHENO, label='...', size=(30, 25))
         hbox3.Add(browseFile3, flag=wx.LEFT, border=8)
         self.vBox.Add(hbox3, flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, border=10)
         self.vBox.Add((-1, 10))
 
-
+        # Choose pheno data column
         hbox4 = wx.BoxSizer(wx.HORIZONTAL)
         text = "Which column represents the phenotype data?"
         st1 = wx.StaticText(self.Panel, label=text)
@@ -62,8 +69,8 @@ class PlotGraphFrame(wx.Frame):
         self.vBox.Add((-1, 25))
 
         hbox5 = wx.BoxSizer(wx.HORIZONTAL)
-        btn2 = wx.Button(self.Panel, label='Cancel', size=(70, 30))
-        self.Bind(wx.EVT_MENU, self.OnClose, btn2)
+        btn2 = wx.Button(self.Panel, id=identityCodes.PLOT_CLOSE, label='Cancel', size=(70, 30))
+
         hbox5.Add(btn2, flag=wx.LEFT | wx.BOTTOM, border=5)
 
         btn1 = wx.Button(self.Panel, label='Plot', size=(70, 30))
@@ -72,9 +79,27 @@ class PlotGraphFrame(wx.Frame):
 
         self.Panel.SetSizer(self.vBox)
 
+        self.Bind(wx.EVT_BUTTON, self.OnBrowseFam, id=identityCodes.PLOT_BROWSE_FAM)
+        self.Bind(wx.EVT_BUTTON, self.OnBrowsePheno, id=identityCodes.PLOT_BROWSE_PHENO)
+        self.Bind(wx.EVT_BUTTON, self.OnClose, id=identityCodes.PLOT_CLOSE)
+
     def OnClose(self, event):
-        print("I must perform boshido")
         self.Close()
+
+    def OnBrowseData(self, event):
+        self.dataFile = browseFiles.get_file_dir(self, "Data File", ".Q.*")
+        if self.dataFile:
+            self.dataTC.write(self.dataFile)
+
+    def OnBrowseFam(self, event):
+        self.famFile = browseFiles.get_file_dir(self, "Fam File", ".fam")
+        if self.famFile:
+            self.famTC.write(self.famFile)
+
+    def OnBrowsePheno(self, event):
+        self.phenotypeFile = browseFiles.get_file_dir(self, "Phenotype File", ".phe")
+        if self.phenotypeFile:
+            self.phoneTC.write(self.phenotypeFile)
 
     def GetColumnList(self, numCols):
         cols = []
